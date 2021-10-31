f9448e336263ca3c0ca32b3ba1e398fc7bdc9db7
Jeremy Gayed <tizmagik@users.noreply.github.com>
2021-10-31 00:51:37 -0400

    Add in options

---
+++ example/LoadMeToo.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+throw new Error('Boom!');
+
+export default function LoadMeToo() {
+  return <div>Loaded me too! {Math.random()}</div>;
+}
--- example/index.tsx
+++ example/index.tsx
@@ -2,11 +2,25 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import asyncComponent from '../.';
 
-const LoadMeAsync = asyncComponent(() => import('./LoadMeAsync'));
+const options = {
+  Loading: () => <div>Loading...</div>,
+  onLoaded: (c) => {
+    console.log('Successfully loaded:', c);
+  },
+  ErrorComponent: () => <div>An error occurred</div>,
+  onError: (e) => {
+    console.error('Error Loading:', e);
+  },
+};
+
+const FailToLoadMe = asyncComponent(() => import(`./LoadMeToo`), options);
+
+const LoadMeAsync = asyncComponent(() => import('./LoadMeAsync'), options);
 
 const App = () => {
   return (
     <div>
+      <FailToLoadMe />
       <LoadMeAsync />
     </div>
   );
